a0999358e518888f50821a632e76b96d64e2c5ad
cXp1r <2510037056@qq.com>
2026-05-11 01:23:51 +0800

    feat: 前端日志添加调用处

--- src/logger.ts
+++ src/logger.ts
@@ -11,16 +11,33 @@ function n2t(level: number) {
 }
 export function initLogLevel() {
     invoke<number>("get_log_level_num").then(l => {
+        
         console.log(`[Logger][DEBUG][${Math.floor(Date.now() / 1000)}]`, "log level:", n2t(l));
         logLevel = l;
     });
 }
 function log(level: number, tag: string, ...args: any[]) {
-    if (logLevel > level) {
-        return;
-    }
-    
-    console.log(`[${tag}][${n2t(level)}][${Math.floor(Date.now() / 1000)}]`, ...args);
+  if (logLevel > level) return;
+
+  const err = new Error();
+  const stack = err.stack;
+  if (!stack) return;
+
+  const lines = stack.split("\n");
+  const targetLine = lines[3] || "";
+  const match = targetLine.match(
+    /at\s+(.+?)\s+\((?:.*\/)?([^\/?#]+)\?[^:]*:(\d+):(\d+)\)/
+  );
+  let atInfo = "";
+  if (match) {
+    atInfo = `|| at ${match[1]}() ${match[2]}:${match[3]}:${match[4]}`;
+  }
+
+  console.log(
+    `[${tag}][${n2t(level)}][${Math.floor(Date.now() / 1000)}]`,
+    ...args,
+    atInfo
+  );
 }
 
 export const loge = (tag: string, ...args: any[]) => log(3, tag, ...args);
